05ed30ce0ade65554f3e541e38d7884d9167118d
S-Wilky <wilkinsonshane12@gmail.com>
2025-05-12 19:46:50 -0700

    New Javascript Project

---
+++ src/functions/testFunc.js
@@ -0,0 +1,9 @@
+import * as dfd from "danfojs-node"
+
+export function test1() {
+    //s = new dfd.Series([1, 3, 5, undefined, 6, 8])
+    //s.print()
+    document.print("test")
+}
+
+export default test1;
---
+++ src/test.js
@@ -0,0 +1,14 @@
+
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <p>
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+// const testing = require("./functions.testFunc")
+
+// testing.test1();
+
+document.querySelector('#app').innerHTML
